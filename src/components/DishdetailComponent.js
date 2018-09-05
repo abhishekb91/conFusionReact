348426e9f0d2751f6162c927d61adcc451d9a2d8
Abhishek Bhandari <abhishekbhandari1991@gmail.com>
2018-09-05 00:11:31 -0400

    added dish detail component

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    renderDish (dish) {
+        return (
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+
+    renderComments (comments) {
+        return comments.map((comment) => {
+            return (
+                <li key={comment.id} className="list-unstyled">
+                    <p>-- {comment.author}, {comment.date}</p>
+                    <p>{comment.comment}</p>
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul>
+                        {this.renderComments(this.props.dish.comments)}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
